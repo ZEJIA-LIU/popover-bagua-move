44c658bdb181c1af19b3203da8bf01100076664c
Leo <790868516@qq.com>
2019-08-13 01:02:27 +0800

    版本二

--- main.js
+++ main.js
@@ -1,45 +1,20 @@
 let $button = document.querySelector(".wrapper>button")
 let $wrapper = document.querySelector(".wrapper")
-let $box = document.querySelector("#box")
+let Box = document.getElementById('Box')
 let ButtonSwitch = true
-let dragging = false
-let X
-let Y
-$box.addEventListener('mousedown', (e) => {
-    dragging = true
-    X = e.clientX
-    Y = e.clientY
-    ButtonSwitch = false
-})
-document.addEventListener('mousemove', (e) => {
-    let moveX = e.clientX - X
-    let moveY = e.clientY - Y
-    if (dragging === true) {
-        $button.style.transform = $button.style.transform + `translate(${moveX}px,${moveY}px)`
-    }
-    X = e.clientX
-    Y = e.clientY
-})
-
-document.addEventListener('mouseup', () => {
-    dragging = false
-    ButtonSwitch = true
-})
-
-
 $button.addEventListener('click', () => {
     if (ButtonSwitch === true) {
         popover.style.display = "block"
-        BoxSwitch.testContest = "点击关闭八卦"
+        Box.textContent = "点击关闭八卦"
         ButtonSwitch = false
     } else {
         popover.style.display = "none"
-        BoxSwitch.testContest = "点击开启八卦"
+        Box.textContent = "点击开启八卦"
         ButtonSwitch = true
     }
     let ClickDocument = function (e) {
         popover.style.display = "none"
-        BoxSwitch.testContest = "点击开启八卦"
+        Box.textContent = "点击开启八卦"
         ButtonSwitch = true
         document.removeEventListener(
             'click', ClickDocument)
