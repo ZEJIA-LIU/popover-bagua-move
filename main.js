3b75e7542a836e46738745c80239894aee643bc1
Leo <790868516@qq.com>
2019-08-14 10:02:44 +0800

    可以拉动了

--- main.js
+++ main.js
@@ -1,8 +1,57 @@
 let $button = document.querySelector(".wrapper>button")
 let $wrapper = document.querySelector(".wrapper")
-let Box = document.getElementById('Box')
+let Box = document.querySelector("#Box")
+let popover = document.querySelector("#popover")
+let dragging = false
+let X
+let Y
+
 let ButtonSwitch = true
+let checkIsMove = false
+
+Box.addEventListener('mousedown', (e) => {
+    dragging = true
+    X = e.clientX
+    Y = e.clientY
+})
+document.addEventListener('mousemove', (e) => {
+    let moveX
+    let moveY
+    moveX = e.clientX - X
+    moveY = e.clientY - Y
+    if ((Math.abs(moveX) >= 2 || Math.abs(moveY) >= 2) && dragging) {
+        checkIsMove = true
+    }
+    var BoundingBox = Box.getBoundingClientRect()
+    var BoundingPopover = popover.getBoundingClientRect()
+    if (dragging === true) {
+        Box.style.transform = Box.style.transform +
+            `translate(${moveX}px,${moveY}px)`
+
+        X = e.clientX
+        Y = e.clientY
+
+    }
+
+    console.log(BoundingBox.right)
+    console.log(BoundingPopover.left)
+
+
+})
+
+document.addEventListener('mouseup', (e) => {
+    dragging = false
+    setTimeout(() => {
+        checkIsMove = false
+    }, 0)
+
+
+})
+
+
 $button.addEventListener('click', () => {
+    if (checkIsMove === true) { return }
+
     if (ButtonSwitch === true) {
         popover.style.display = "block"
         Box.textContent = "点击关闭八卦"
@@ -23,5 +72,4 @@ $button.addEventListener('click', () => {
 })
 $wrapper.addEventListener('click', (e) => {
     e.stopPropagation()
-
 })
